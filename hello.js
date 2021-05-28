183077fe678b171bfba5e3a49d45230f526b3205
anshal ashish <91808@LAPTOP-ICEIOISQ. (none)>
2021-05-28 14:19:09 +0530

    initial commit

---
+++ hello.js
@@ -0,0 +1,73 @@
+// console.log('hello from javascript');
+// var personName = 'Global: dave';
+// {
+//   let personName = 'Scoped: philip';
+//   let personMarks = 10;
+//   personMarks = personMarks + 20;
+//   const personBloodGroup = 'B+';
+//   personBloodGroup = 'A+';
+//   console.log('inside block');
+//   console.log(personName);
+//   console.log(personMarks);
+//   // console.log("Sco");
+// }
+
+// //let and const
+// //scope of variable
+
+// console.log('Global', personName);
+// function printName() {
+//   console.log('inside function', personName);
+// }
+// printName();
+
+// //functions
+// //
+
+// sum = (num1, num2) => {
+//   return num1 + num2;
+// };
+// let result = sum(20, 30);
+// console.log(result);
+
+//class
+// class Car {
+//   name;
+//   model;
+//   engine;
+//   constructor(name, model, engine) {
+//     this.name = name;
+//     this.model = model;
+//     this.engine = engine;
+//   }
+
+//   age() {
+//     console.log('inside age');
+//     console.log(this);
+//     let date = new Date();
+//     return date.getFullYear() - this.model;
+//   }
+// }
+
+// let myFord = new Car('FordMustang', 1997, '2500');
+// console.log(myFord.age());
+
+//JSON
+var myDetails = {
+  name: 'anshal',
+  age: '22',
+  bg: 'A-',
+};
+
+JSON.parse;
+console.log(myDetails);
+
+//event
+
+//array method
+
+//loop
+
+//hoisting
+
+//
